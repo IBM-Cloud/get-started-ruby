5a756b092342e9198746775c71fccc440b15fa2e
Ram Vennam <rvennam987@gmail.com>
2017-02-21 22:31:55 -0600

    add VCAP_SERVICES and local config

--- Gemfile
+++ Gemfile
@@ -2,10 +2,10 @@ source 'https://rubygems.org'
 ruby "2.3.0"
 gem 'rails', '~> 3.2.22'
 
+gem 'dotenv-rails', groups: [:development, :test]
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
-
-
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -19,8 +19,8 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
-gem 'jquery-rails'
 gem 'couchrest'
+
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/api/visitors_controller.rb
+++ app/controllers/api/visitors_controller.rb
@@ -1,37 +1,58 @@
 require 'couchrest'
 require 'rubygems'
 require 'json'
-require 'sinatra'
+require 'dotenv'
+
+Dotenv.load()
 
 module Api
   class VisitorsController < ApplicationController
     protect_from_forgery with: :null_session
     before_filter :init_db
-	
-	#uri = 'mongodb://localhost:27017'
-    #database = 'db'
 
-	#db=nil
- 
-#    def get_couch_db(creds)
-#	  url = creds['url']
-#	  if !url.end_with?('/')
-#		url = url + '/'
-#	  end
-#      url = url + 'mydb'
-#      puts 'Using URL: ' + url
-#      db = CouchRest.database!(url)
-#    end
- 
     def init_db
-      @db = CouchRest.database!("https://03368d13-7aac-42c8-a9a4-f77ed3d7ab80-bluemix:3f85a56c0963be9b09027a522634077c4a1b6393f533f45f170706dd2abb6722@03368d13-7aac-42c8-a9a4-f77ed3d7ab80-bluemix.cloudant.com/mydb")
-    # if ENV['VCAP_SERVICES']
-    #   svcs = JSON.parse ENV['VCAP_SERVICES']  
-    #   couchrest = svcs.detect { |k,v| k =~ /^ruby-rails/ }.last.first
-    #   creds = couchrest['credentials']
-    #   uri = creds['url']
-    #   @db = uri[%r{/([^/\?]+)(\?|$)}, 1]
-    # end
+      # @db = CouchRest.database!("https://03368d13-7aac-42c8-a9a4-f77ed3d7ab80-bluemix:3f85a56c0963be9b09027a522634077c4a1b6393f533f45f170706dd2abb6722@03368d13-7aac-42c8-a9a4-f77ed3d7ab80-bluemix.cloudant.com/mydb")
+      if ENV['VCAP_SERVICES']
+        svcs = JSON.parse ENV['VCAP_SERVICES']
+        cloudant = svcs.detect { |k,v| k =~ /^cloudantNoSQLDB/ }.last.first
+        creds = cloudant['credentials']
+        @db = get_couch_db(creds)
+      else
+        @db = CouchRest.database!(ENV['CLOUDANT_URL'])
+      end
+      create_view(@db)
+    end
+
+    # Helper function to construct the proper URL
+    # to the Couch DB.
+    # URL should be http(s)://username:password@user.cloudant.com/mydb
+    def get_couch_db(creds)
+      url = creds['url']
+      if !url.end_with?('/')
+        url = url + '/'
+      end
+      url = url + 'mydb'
+
+      puts 'Using URL: ' + url
+      #This will create the DB if it does not exist, however it will fail if you do not have permissions
+      CouchRest.database!(url)
+    end
+
+    # Helper function to create the view if it does not already exist
+    def create_view(db)
+      begin
+        db.get('_design/todos')
+      rescue RestClient::ResourceNotFound => nfe
+        db.save_doc({
+          "_id" => "_design/todos",
+          :views => {
+            :allTodos => {
+              :reduce => "_count",
+              :map => "function(doc){if(doc.name != null){emit(doc.order,{name: doc.name})}}"
+            }
+          }
+          })
+      end
     end
 
     # POST /api/visitors
